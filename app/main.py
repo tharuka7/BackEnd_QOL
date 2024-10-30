863781df883f505819cb355296fbbdd9218335fa
Oshen Madhuka <oshenbhawajith1999@gmail.com>
2024-10-31 04:58:21 +0530

    adding new

--- app/main.py
+++ app/main.py
@@ -7,16 +7,16 @@ from app.routes import router
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allows requests from your frontend
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
 )
 
 # Include the router
